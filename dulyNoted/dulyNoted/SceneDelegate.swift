5f18d8a14d28bb5262cf9ffe44769560357bdd80
Alex McCall <61480140+d1stract3d@users.noreply.github.com>
2020-05-16 13:14:41 -0500

    reUpload

--- dulyNoted/dulyNoted/SceneDelegate.swift
+++ dulyNoted/dulyNoted/SceneDelegate.swift
@@ -3,14 +3,12 @@
 //
 //  Created by Alex McCall.
 //  Copyright © 2020 Alex McCall. All rights reserved.
-//
 
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
@@ -47,6 +45,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
-
 }
-
